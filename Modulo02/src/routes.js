1b90e4a211ed432743d9360696d47a55bb230c0e
marcioestevao <marcioestevao@msn.com>
2019-08-26 20:45:20 -0300

    Criando o loader de models - criação de dados do usuário no banco de dados

--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World Updated' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcio Estevão',
+    email: 'marcioestevao@msn.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
