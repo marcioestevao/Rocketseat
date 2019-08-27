9725c854bea665db0a2f2fec3641e2146123eb68
marcioestevao <marcioestevao@msn.com>
2019-08-26 21:11:08 -0300

    Cadastro de usuários - via API

---
+++ Modulo02/src/app/controllers/UserController.js
@@ -0,0 +1,32 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // Tratamento para verificar se o email do usuário ja existe
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    // Deste modo retorna sempre todos os campos criados na tabela
+    /*
+    const user = await User.create(req.body);
+
+    return res.json(user);
+    */
+
+    // Se desejar que só retorne alguns campos (evitando trafegar informações
+    // desnecessárias), faz-se da seguinte maneira:
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
