ee157f48cc260c1709c75026b1bba1e30392460b
marcioestevao <marcioestevao@msn.com>
2019-08-28 20:20:24 -0300

    Middleware de autenticação - Criado rota update usuário com acesso livre

--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -7,5 +7,6 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
