3c6c4dfd689b5b2b5af7da98c29f6b60278d182a
marcioestevao <marcioestevao@msn.com>
2019-11-15 11:48:39 -0300

    Utilizando Class Components

--- Modulo04/src/App.js
+++ Modulo04/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/profile.jpg";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="150" src={profile}></img>;
+  return <TechList />;
 }
 
 export default App;
